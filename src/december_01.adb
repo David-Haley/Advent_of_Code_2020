ba0b62017938246d3b3c14d30dc9698aa5260a54
David-Haley <david.haley@bigpond.com>
2020-12-01 17:50:40 +1000

    Update december_01.adb
    Obviously have too much spare time. Made the code "better" loop limits symetrical. May look like loops will run more time, only outer loop will run for Last_Index, Some comments added to show the assumptions on which the solution is based. Simple problem but it would be interesting to see if Spark's theorem prover could prove the solution correct.

--- src/december_01.adb
+++ src/december_01.adb
@@ -6,33 +6,41 @@ procedure December_01 is
 
    Input_File : File_Type;
 
-   package Expenses is new Ada.Containers.Vectors (Positive, Natural);
+   -- Assumption there are no 0 expenses
+   package Expenses is new Ada.Containers.Vectors (Positive, Positive);
    use Expenses;
    Expense : Expenses.Vector := Empty_Vector;
+
+   Year_2020 : constant Positive := 2020;
 
 begin -- December_01
    Open (Input_File, In_File, "december_01.txt");
    while not End_Of_File (Input_File) loop
-      Append (Expense, Natural'Value (Get_Line (Input_File)));
+      Append (Expense, Positive'Value (Get_Line (Input_File)));
    end loop; -- End_Of_File (Input_File)
    Close (Input_File);
-   for I in Positive range 1 .. Last_Index (Expense) - 1 loop
+   for I in Positive range 1 .. Last_Index (Expense) loop
       for J in Positive range I + 1 .. Last_Index (Expense) loop
-         if Expense (I) + Expense (J) = 2020 then
-            Put_Line ("Part one :" & Natural'Image (Expense (I)) & " *" &
-                        Natural'Image (Expense (J)) & " =" &
-                        Natural'Image (Expense (I) * Expense (J)));
+         -- I < J therefore if I = Last_Index the J loop does not execute
+         if Expense (I) + Expense (J) = Year_2020 then
+            Put_Line ("Part one :" & Positive'Image (Expense (I)) & " *" &
+                        Positive'Image (Expense (J)) & " =" &
+                        Positive'Image (Expense (I) * Expense (J)));
          else
+            -- On the assumption there are no zero expenses once two expenses
+            -- add to the required result adding a third can never equal the
+            -- required result.
             for K in Positive range J + 1 .. Last_Index (Expense) loop
-               if Expense (I) + Expense (J) + Expense (K) = 2020 then
-                  Put_Line ("Part two :" & Natural'Image (Expense (I)) & " *" &
-                              Natural'Image (Expense (J)) & " *" &
-                              Natural'Image (Expense (K)) & " =" &
-                              Natural'Image (Expense (I) *
+               -- J > I and K > J implies K > I, that is, I, J and K are unique
+               if Expense (I) + Expense (J) + Expense (K) = Year_2020 then
+                  Put_Line ("Part two :" & Positive'Image (Expense (I)) & " *" &
+                              Positive'Image (Expense (J)) & " *" &
+                              Positive'Image (Expense (K)) & " =" &
+                              Positive'Image (Expense (I) *
                                 Expense (J) * Expense (K)));
-               end if; -- Expense (I) + Expense (J) + Expense (K) = 2020
-            end loop; -- K in Positive range I + 2 .. Last_Index (Expense)
-         end if; -- Expense (I) + Expense (J) = 2020
+               end if; -- Expense (I) + Expense (J) + Expense (K) = Year_2020
+            end loop; -- K in Positive range J + 1 .. Last_Index (Expense)
+         end if; -- Expense (I) + Expense (J) = Year_2020
       end loop; -- J in Positive range I + 1 .. Last_Index (Expense)
-   end loop; -- I in Positive range 1 .. Last_Index (Expense) - 1
+   end loop; -- I in Positive range 1 .. Last_Index (Expense)
 end December_01;
