c57458d906fe5ce8e82f01ae2f69b04685ec13e1
David-Haley <34273510+David-Haley@users.noreply.github.com>
2020-12-11 11:53:32 +1000

    Update december_10.adb
    Solved part two, replaced recursive search which resolved each combination, by adding up count of the combination at the 3 levels below.

--- src/december_10.adb
+++ src/december_10.adb
@@ -1,8 +1,5 @@
 with Ada.Text_IO; use  Ada.Text_IO;
-with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Ada.Containers; use Ada.Containers;
-with Ada.Containers.Ordered_Sets;
+with Ada.Containers.Ordered_Maps;
 
 procedure December_10 is
 
@@ -10,65 +7,67 @@ procedure December_10 is
 
    package Jolt_IO is new Ada.Text_IO.Integer_IO (Jolts);
 
-   package Adaptor_Sets is new Ada.Containers.Ordered_Sets (Jolts);
-   use Adaptor_Sets;
+   subtype Options is Long_Long_Integer range 0 .. Long_Long_Integer'Last;
+
+   package Adaptor_Maps is new Ada.Containers.Ordered_Maps (Jolts, Options);
+   use Adaptor_Maps;
 
    Source_Jolts : constant Jolts := 0;
    Tolerance : constant Jolts := 3;
 
-   procedure Search_Options (Available_In : in Adaptor_Sets.Set;
-                             Source_Jolts : in Jolts;
-                             Target_Jolts : in Jolts;
-                             Count : in out Long_Long_Integer) is
+   procedure Build_Options (Adaptor_Map : in out Adaptor_Maps.Map) is
 
-      Available : Adaptor_Sets.Set := Copy (Available_In);
+      Count : Options;
+      Lower, Upper : Jolts;
 
-   begin -- Search_Options
-      if Source_Jolts + Tolerance = Target_Jolts then
-         Count := Count + 1;
-      else
-         for J in Jolts range Source_Jolts + 1 .. Source_Jolts + Tolerance loop
-            if Contains (Available, J) then
-               Exclude (Available, J);
-               Search_Options (Available, J, Target_Jolts, Count);
-               Include (Available, J);
-            end if; -- Contains (Available, J)
-         end loop; -- J in Jolts range Source_Jolts .. Source_Jolts + Tolerance
-      end if; -- Source_Jolts + Tolerance = Target_Jolts
-   end Search_Options;
+   begin -- Build_Options
+      for A in Iterate (Adaptor_Map) loop
+         Count := 0;
+         if Key (A) - Tolerance < Source_Jolts then
+            Lower := Source_Jolts;
+         else
+            Lower := Key (A) - Tolerance;
+         end if; -- Key (A) - Tolerance < Source_Jolts
+         if Key (A) - 1 < Source_Jolts then
+            Upper := Source_Jolts;
+         else
+            Upper := Key (A) - 1;
+         end if; -- Adaptor_Map (A) - 1 < Source_Jolts
+         for J in Jolts range Lower .. Upper loop
+            if Contains (Adaptor_Map, J) then
+               Count := Count + Adaptor_Map (J);
+            end if; -- Contains (Adaptor_Map, J)
+         end loop; -- J in Jolts range Key (A) - Tolerance .. Key (A) - 1
+         Adaptor_Map (A) := Count;
+      end loop; -- A in Iterate (Adaptor_Map)
+   end Build_Options;
 
    Input_File : File_Type;
-   Text : Unbounded_String;
-   Adaptor_Set : Adaptor_Sets.Set := To_Set (Source_Jolts);
-   Adaptor : Jolts;
-   Target_Jolts : Jolts;
+   Adaptor_Map : Adaptor_Maps.Map;
+   Adaptor, Device_Jolts : Jolts;
    Difference_1, Difference_3 : Natural := 0;
-   Count : Long_Long_Integer := 0;
 
 begin -- December_10
+   Include (Adaptor_Map, Source_Jolts, 1);
    Open (Input_File, In_File, "december_10.txt");
-   -- Open (Input_File, In_File, "example_1_10.txt");
-   -- Open (Input_File, In_File, "example_2_10.txt");
    while not End_Of_File (Input_File) loop
       Jolt_IO.Get (Input_File, Adaptor);
       Skip_Line (Input_File);
-      Include (Adaptor_Set, Adaptor);
+      Include (Adaptor_Map, Adaptor, Options'First);
    end loop; -- not End_Of_File (Input_File)
-   Target_Jolts := Last_Element (Adaptor_Set) + Tolerance;
-   Include (Adaptor_Set, Target_Jolts);
-   for A in Iterate (Adaptor_Set) loop
+   Device_Jolts := Last_Key (Adaptor_Map) + Tolerance;
+   Include (Adaptor_Map, Device_Jolts, Options'First);
+   for A in Iterate (Adaptor_Map) loop
       if Next (A) /= No_Element then
-         if Element (Next (A)) - Element (A) = 1 then
+         if Key (Next (A)) - Key (A) = 1 then
             Difference_1 := Difference_1 + 1;
-         elsif Element (Next (A)) - Element (A) = 3 then
+         elsif Key (Next (A)) - Key (A) = 3 then
             Difference_3 := Difference_3 + 1;
          end if; -- Element (Next (I)) - Element (I) = 1
       end if; -- Next (A) /= No_Element
-   end loop; -- A in Iterate (Adaptor_Set)
-   Put_Line ("Difference_1:" & Jolts'Image (Difference_1));
-   Put_Line ("Difference_3:" & Jolts'Image (Difference_3));
-   Put_Line ("Product:" & Jolts'Image (Difference_1 * Difference_3));
-   Search_Options (Adaptor_Set, Source_Jolts, Target_Jolts, Count);
-   Put_Line ("Option count:" & Long_Long_Integer'Image (Count));
+   end loop; -- A in Iterate (Adaptor_Map)
+   Put_Line ("Part one:" & Jolts'Image (Difference_1 * Difference_3));
+   Build_Options (Adaptor_Map);
+   Put_Line ("Part two:" & Options'Image (Element (Adaptor_Map, Device_Jolts)));
    Close (Input_File );
 end December_10;
